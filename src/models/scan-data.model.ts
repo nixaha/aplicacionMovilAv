10bfb29bcd07a1aaeede5cc32880e7beb2ea9b15
nixaha <nixaha_saucedo@hotmail.com>
2018-05-29 12:17:58 -0500

    creacion de proyecto practica

---
+++ src/models/scan-data.model.ts
@@ -0,0 +1,8 @@
+//export class ScanData{
+    //info:string;
+    //tipo:string;
+     
+     //constructor(tupoArchivo) {
+     //this.tipo = tipoArchivo;        
+  //}
+//}
---
+++ src/pages/index.paginas.ts
@@ -0,0 +1,3 @@
+export { AboutPage } from '../pages/about/about';
+export { HomePage } from '../pages/home/home';
+export { TabsPage } from '../pages/tabs/tabs';
